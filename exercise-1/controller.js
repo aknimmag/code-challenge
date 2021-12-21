bfc8435449d786cf23566835ed65d06ec0636547
DIR\akhil.nimmagadda <ak.nimmagadda@gmail.com>
2021-12-21 13:05:24 -0500

    Controller changes added!

--- exercise-1/controller.js
+++ exercise-1/controller.js
@@ -1,25 +1,42 @@
 // api url
 const api_url =
-	"https://615485ee2473940017efaed3.mockapi.io/assessment";
+	'https://615485ee2473940017efaed3.mockapi.io/assessment';
 
-// Defining async function
-async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
+// Defining function
+	function getUserInfo() {
+		fetch(api_url)
+			.then(response => response.json())
+			.then(data => {
+				displayData(data)
+			});
 	}
-    console.log(data);
-	show(data);
+
+// Function to define innerHTML for HTML unordered list
+function displayData(data) {
+	console.log('raw data->', data);
+    const list = document.getElementById('afUsers');
+    const unOrderedData = `<ul> 
+                {{#each data}} 
+                <li>
+                 <div> 
+                 <h4> Name: <strong> {{this.name}} </strong> </h4> 
+                 <img src={{this.avatar}} alt= 'Image not available'> 
+                 <button type=button onclick='clickHandler({{this.id}})'>More info</button>
+                 <p id={{this.id}} style = 'display: none'> ID : {{this.id}} is created at {{this.createdAt}} </p>
+                 </div>
+                 </li>
+                 {{/each}} 
+                 </ul>`;
+    const template = Handlebars.compile(unOrderedData);
+    list.innerHTML = template({ data: data });
 }
-// Calling that async function
-getapi(api_url);
 
-// Function to define innerHTML for HTML table
-
-
+// Click handler
+function clickHandler(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === 'none') { // Click func.
+        x.style.display = 'block';
+    } else { // Hide
+        x.style.display = 'none';
+    }
+}
